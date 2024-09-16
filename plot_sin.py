4aef5adeef7cde0aaebbce7358a3b42201556462
Oscar Benjamin <oscar.j.benjamin@gmail.com>
2024-09-16 14:37:43 +0100

    remove comments. add title

--- plot_sin.py
+++ plot_sin.py
@@ -1,12 +1,6 @@
-# -*- coding: utf-8 -*-
-"""
-Spyder Editor
-
-This is a temporary script file.
-"""
-
 import matplotlib.pyplot as plt
 
 plt.plot([0, 1, 4], [0, 1, -1])
+plt.title('things go up and down')
 plt.show()
 
